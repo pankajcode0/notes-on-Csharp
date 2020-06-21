25134c6881c83e7d166dfe9199738658c6e9c455
pankaj kumar <pankajcode01@gmail.com>
2020-06-21 19:59:48 +0530

    helloWorld

--- helloWorld.cs
+++ helloWorld.cs
@@ -1,3 +1,9 @@
+//Running c# on ubuntu using mono.
+// date 21 june 2019
+
+// for compiling use : mcs -out:helloWorld.exe helloWorld.cs
+// for executing the code use : mono helloWorld.exe
+
 using System;
 
 namespace Project_1 {
